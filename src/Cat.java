d050cbc6b929e4f40d36d15731a18d6894876464
Simao <s.carrasco@campus.fct.unl.pt>
2022-09-17 17:00:18 +0100

    Added the Animal interface.

---
+++ src/Cat.java
@@ -0,0 +1,19 @@
+public class Cat implements Animal {
+
+    private static final String MEOW= "Meow!";
+    private String name;
+
+    public Cat(String name){
+        this.name=name;
+    }
+
+    @Override
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String speak(){
+        return Cat.MEOW;
+    }
+}
